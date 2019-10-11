13a66c02ad2088e20e2268b14a7182338afbb386
Michael Cheng <mcheng@us.ibm.com>
2019-10-11 08:32:36 -0400

    initial push

---
+++ Dockerfile
@@ -0,0 +1,71 @@
+# Copyright 2019 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Production build dockerfile
+# Stage 1: build using golang image
+FROM golang as builder
+
+WORKDIR $GOPATH/src/github.com/kabanero-webhook
+
+# install dep tool
+RUN curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+
+# Copy dependecies over
+COPY  Gopkg.* ./
+
+# load vendor dependent packages
+RUN dep ensure -v -vendor-only
+
+# Copy source over
+COPY *.go ./
+
+# Linter
+RUN go get -u golang.org/x/lint/golint; golint -set_exit_status
+
+# Run unit test
+# COPY test_data ./test_data/
+# RUN go test -v
+
+# Build executable
+RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"'
+
+# Stage 2: Build official image based on UBI
+FROM registry.access.redhat.com/ubi7-minimal:7.6-123
+
+LABEL name="Kabanero Webhook" \
+      version="0.1.0" \
+      release="0.1.0" \
+      vendor="kabanero" \
+      summary="Kabanero Webhook" \
+      description="Image for Kabanero webhook processing"
+
+RUN microdnf -y install shadow-utils \
+    && microdnf clean all \
+    && mkdir /licenses \
+    && useradd -u 1001 -r -g 0 -s /usr/sbin/nologin default \
+    && microdnf -y remove shadow-utils \
+    && microdnf clean all \
+    && mkdir /app \
+    && chown 1001:0 /app \
+    && chmod g+rwx /app
+
+COPY --from=builder --chown=1001:0 /go/src/github.com/kabanero-webhook/kabanero-webhook /app
+COPY --chown=1001:0 licenses/ /licenses/
+# COPY --chown=1001:0 testcntlr.sh /bin/testcntlr.sh
+USER 1001
+WORKDIR /app
+
+# run with log level 2
+# Note liveness/readiness probe depends on './webhook'
+CMD ./kabanero-webhook -v 2
---
+++ main.go
@@ -0,0 +1,185 @@
+/*
+Copyright 2019 IBM Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"syscall"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog"
+)
+
+const (
+	kubeAPIURL = "http://localhost:9080"
+)
+
+var (
+	masterURL  string        // URL of Kube master
+	kubeconfig string        // path to kube config file. default <home>/.kube/config
+	klogFlags  *flag.FlagSet // flagset for logging
+)
+
+func init() {
+	// Print stacks and exit on SIGINT
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT)
+		buf := make([]byte, 1<<20)
+		<-sigChan
+		stacklen := runtime.Stack(buf, true)
+		klog.Infof("=== received SIGQUIT ===\n*** goroutine dump...\n%s\n*** end\n", buf[:stacklen])
+		os.Exit(1)
+	}()
+}
+
+func main() {
+
+	flag.Parse()
+
+	var cfg *rest.Config
+	var err error
+	if strings.Compare(masterURL, "") != 0 {
+		// running outside of Kube cluster
+		klog.Infof("starting Kabanero webhook outside cluster\n")
+		klog.Infof("masterURL: %s\n", masterURL)
+		klog.Infof("kubeconfig: %s\n", kubeconfig)
+		cfg, err = clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+		if err != nil {
+			klog.Fatal(err)
+		}
+	} else {
+		// running inside the Kube cluster
+		klog.Infof("starting Kabanero webhook status controler inside cluster\n")
+		cfg, err = rest.InClusterConfig()
+		if err != nil {
+			klog.Fatal(err)
+		}
+	}
+
+	var kubeClient *kubernetes.Clientset
+	kubeClient, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	var discClient = kubeClient.DiscoveryClient
+	var dynamicClient dynamic.Interface
+	dynamicClient, err = dynamic.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatal(err)
+	}
+	klog.Infof("Received discClient %T, dynamicClient  %T\n", discClient, dynamicClient)
+
+	// gvr := schema.GroupVersionResource { Group: "app.k8s.io", Version: "v1beta1", Resource: "applications" }
+	// deleteOrphanedAutoCreatedApplications(dynamicClient, gvr )
+
+	// plugin := &ControllerPlugin{dynamicClient, discClient, DefaultBatchDuration, calculateComponentStatus}
+	// resController, err := NewClusterWatcher(plugin)
+	// _, err = NewClusterWatcher(plugin)
+	// if err != nil {
+	//	klog.Fatal(err)
+	//}
+
+	select {}
+}
+
+func printEvent(event watch.Event) {
+	klog.Infof("event type %s, object type is %T\n", event.Type, event.Object)
+	printEventObject(event.Object, "    ")
+}
+
+func printEventObject(obj interface{}, indent string) {
+	switch obj.(type) {
+	case *unstructured.Unstructured:
+		var unstructuredObj = obj.(*unstructured.Unstructured)
+		// printObject(unstructuredObj.Object, indent)
+		printUnstructuredJSON(unstructuredObj.Object, indent)
+		klog.Infof("\n")
+	default:
+		klog.Infof("%snot Unstructured: type: %T val: %s\n", indent, obj, obj)
+	}
+}
+
+func printUnstructuredJSON(obj interface{}, indent string) {
+	data, err := json.MarshalIndent(obj, "", indent)
+	if err != nil {
+		klog.Fatalf("JSON Marshaling failed %s", err)
+	}
+	klog.Infof("%s\n", data)
+}
+
+func printObject(obj interface{}, indent string) {
+	nextIndent := indent + "    "
+	switch obj.(type) {
+	case int:
+		klog.Infof("%d", obj.(int))
+	case bool:
+		klog.Infof("%t", obj.(bool))
+	case float64:
+		klog.Infof("%f", obj.(float64))
+	case string:
+		klog.Infof("%s", obj.(string))
+	case []interface{}:
+		var arr = obj.([]interface{})
+		for index, elem := range arr {
+			klog.Infof("\n%sindex:%d, type %T, ", indent, index, elem)
+			printObject(elem, nextIndent)
+		}
+	case map[string]interface{}:
+		var objMap = obj.(map[string]interface{})
+		for label, val := range objMap {
+			klog.Infof("\n%skey: %s type: %T| ", indent, label, val)
+			printObject(val, nextIndent)
+		}
+	default:
+		klog.Infof("\n%stype: %T val: %s", indent, obj, obj)
+	}
+}
+
+func printPods(pods *v1.PodList) {
+	for _, pod := range pods.Items {
+		klog.Infof("%s", pod.ObjectMeta.Name)
+	}
+}
+
+func init() {
+	// flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	if home := homedir.HomeDir(); home != "" {
+		flag.StringVar(&kubeconfig, "kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+
+	// init falgs for klog
+	klog.InitFlags(nil)
+
+}
